be3a37a8222a39fa57971607944ac27da0b30780
Michael Smith <michael.smith.ok@gmail.com>
2023-07-28 16:08:28 -0500

    Fix styling

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,10 +15,12 @@ setInterval(() => {
 
   if (document.getElementById('open-ar-dialog-btn') && !document.getElementById('open-pnl-send-to-dealer')) {
     const pnl_send_button = document.createElement("button");
-    pnl_send_button.classList.add('MuiButtonBase-root', 'MuiButton-root', 'MuiButton-outlined', 'jss55', 'jss57', 'jss53', 'MuiButton-outlinedPrimary');
+    pnl_send_button.classList.add('MuiButtonBase-root', 'MuiButton-root', 'MuiButton-outlined', 'MuiButton-outlinedPrimary');
     pnl_send_button.type = 'button';
     pnl_send_button.id = 'open-pnl-send-to-dealer';
     pnl_send_button.innerHTML = `<span class="MuiButton-label"><span class="MuiButton-startIcon MuiButton-iconSizeMedium"><svg class="MuiSvgIcon-root" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M18 16.08c-.76 0-1.44.3-1.96.77L8.91 12.7c.05-.23.09-.46.09-.7s-.04-.47-.09-.7l7.05-4.11c.54.5 1.25.81 2.04.81 1.66 0 3-1.34 3-3s-1.34-3-3-3-3 1.34-3 3c0 .24.04.47.09.7L8.04 9.81C7.5 9.31 6.79 9 6 9c-1.66 0-3 1.34-3 3s1.34 3 3 3c.79 0 1.5-.31 2.04-.81l7.12 4.16c-.05.21-.08.43-.08.65 0 1.61 1.31 2.92 2.92 2.92 1.61 0 2.92-1.31 2.92-2.92s-1.31-2.92-2.92-2.92z"></path></svg></span>Send to dealer</span><span class="MuiTouchRipple-root"></span>`;
+    pnl_send_button.style.backgroundColor = 'white';
+    pnl_send_button.style.width = '100%';
     pnl_send_button.onclick = () => {
       sendToDealerStep = 0;
     };
