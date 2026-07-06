8f2043c78ead09665abcdc586454b2550d5c46c0
smwa <smwa@users.noreply.github.com>
2026-07-06 16:50:33 +0000

    deploy: 18453266b4df4dc297477eefa306f29c360ee3ae

---
+++ README.md
@@ -0,0 +1,3 @@
+# P&M Outdoor Structures
+
+Jekyll site for [P&M Outdoor Structures](https://pmoutdoorstructures.com).
--- service-worker.js
+++ service-worker.js
@@ -14,8 +14,8 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = "precache-20260511153409+0000";
-const RUNTIME = "runtime-20260511153409+0000";
+const PRECACHE = "precache-20260706163803+0000";
+const RUNTIME = "runtime-20260706163803+0000";
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
@@ -606,9 +606,9 @@ const PRECACHE_URLS = [
   
     './slideshow/wood/',
   
-    './slideshow/metal/',
-  
     './slideshow/tinyhomes/',
+  
+    './slideshow/metal/',
   
     './build/tn-steel/',
   
